7e0c056dfa790b7dd705c7679e50643287a9ba24
kavyaab2005 <kavya0308181@gmail.com>
2025-11-24 11:24:39 +0530

    Create studentRoutes.js

---
+++ routes/studentRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { addStudent, getStudents } from "../controllers/studentController.js";
+
+const router = express.Router();
+
+router.post("/add", addStudent);
+router.get("/", getStudents);
+
+export default router;
